d39596d7971e05739d980b0ed7e8aab646095c5c
liya3719 <liya3719@gmail.com>
2019-07-17 18:28:09 +0800

    add vue config

--- vue.config.js
+++ vue.config.js
@@ -3,12 +3,24 @@
  */
 const buildUpload = require('./src/common/buildUpload');
 const isProduction = process.env.NODE_ENV === 'production';
-let testAddress;
+let testAddress, targetAddress;
 try {
   testAddress = JSON.parse(process.env.npm_config_argv).original.slice(2).toString();    
 } catch (error) {
   testAddress = process.argv.slice(2).toString();
 };
+let testServer = {
+  test: {
+    receiver: 'http://接收端ip地址/fisreceiver.php',
+    toPath: '/home/homework/webroot/static/你的项目名称'
+  }
+}
+let reg = /^test?(\d+)$/g;
+if(reg.test(testAddress)) {
+   targetAddress = testAddress ? `https://${testAddress}.suanshubang.com` : `本地mock服务地址`;
+} else {
+  targetAddress = testAddress ? `https://${testAddress}-docker.suanshubang.com` : `本地mock服务地址`;
+}
 module.exports = {
   outputDir: process.env.outputDir,
   publicPath:  process.env.publicPath,
@@ -20,6 +32,22 @@ module.exports = {
   pages: {
     index: {
       entry: './src/main.ts'
+    }
+  },
+  devServer: {
+    proxy: {
+      '/kpstaff': {
+        target: !isProduction ? targetAddress : '',
+        ws: true,
+        changeOrigin: true
+      }
+    }
+  },
+  configureWebpack: config => {
+    if(process.env.NODE_ENV === 'production') {
+      if(testAddress.indexOf('test') != -1) {
+        config.plugins.push(new buildUpload(testServer[testAddress]));
+      }
     }
   }
 }
