c331d2f43b6387ed1d2bc54484fb7b8cef14ecfe
李亚 <liya19900719@126.com>
2020-09-01 16:13:40 +0800

    fix: post request

--- src/services/commonService.ts
+++ src/services/commonService.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import qs from 'qs';
 axios.defaults.withCredentials = true;
 /**
  * @class CommonSerivce
@@ -42,7 +43,7 @@ export class CommonService {
         axios({
           url: api,
           method: 'POST',
-          data,
+          data: qs.stringify(data),
           headers: {
             'Content-Type': 'application/x-www-form-urlencoded',
           },
