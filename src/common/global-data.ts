eca1ada4660e042d180af56e7911613f421c6d3b
李亚 <liya@liyadeMacBook-Pro.local>
2021-12-28 11:46:23 +0800

    fix: isLocation

--- src/common/global-data.ts
+++ src/common/global-data.ts
@@ -1,3 +1,3 @@
-export const isLocation = location.hostname.indexOf('location') > -1;
+export const isLocation = location.hostname.indexOf('localhost') > -1;
 export const isDev = process.env.NODE_ENV === 'development';
 export const isProduction = process.env.NODE_ENV === 'production';
