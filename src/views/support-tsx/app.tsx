6dc7d386160814c03f8b70a6bbb62b1d4469d810
liya <liya@zuoyebang.com>
2022-06-08 17:48:33 +0800

    feat: add css modules example

--- src/views/support-tsx/app.tsx
+++ src/views/support-tsx/app.tsx
@@ -1,12 +1,20 @@
 import { Component } from 'vue-property-decorator';
 import * as tsx from 'vue-tsx-support';
+import  { Container } from 'typedi';
+import styles from './app.module.less';
 
-@Component({})
+@Component
 export default class TestView extends tsx.Component<{}> {
   private test = 1;
+  created() {
+    this.handleTestRequest()
+  }
+  private async handleTestRequest() {
+    const result = await Container.get(MainService).getMain();
+  }
   private render() {
     return (
-      <div class='test'>1111</div>
+      <div class={styles.test}>1111</div>
     )
   }
   private mounted() {
