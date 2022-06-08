ef99d82ac76a39753ee3d4dc47ae59b1410c5a5b
liya <liya@zuoyebang.com>
2022-06-08 17:42:05 +0800

    feat: add custom axios data & css modules

--- src/shims-tsx.d.ts
+++ src/shims-tsx.d.ts
@@ -1,5 +1,5 @@
 import Vue, { VNode } from 'vue';
-
+import * as axios from 'axios';
 declare global {
   namespace JSX {
     // tslint:disable no-empty-interface
@@ -9,5 +9,16 @@ declare global {
     interface IntrinsicElements {
       [elem: string]: any;
     }
+  }
+}
+
+declare module 'axios' {
+  // 这里自定义AxiosResponse返回数据
+  export interface AxiosResponse {
+
+  }
+  // 这里自定义AxiosError返回数据
+  export interface AxiosError {
+
   }
 }
